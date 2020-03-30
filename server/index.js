d43e7838020bca333e07709a9840e66f25fe06b3
Mike Lee <mikemunkyu.lee@gmail.com>
2020-03-30 19:44:37 -0400

    Initial commit; scaffold client and server directory structure

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const colors = require('colors');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path');
+
+// import { connectDB } from './config/db';
+// import { router as transactionsRouter } from './routes/transactions';
+
+dotenv.config({ path: './server/config/config.env' });
+
+// connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// app.use('/api/v1/transactions', transactionsRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 4000;
+
+const testCallback = () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  );
+};
+
+app.listen(PORT, testCallback);
